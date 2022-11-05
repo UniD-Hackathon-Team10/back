5db24825714846d20b36f2e09ccb618dfd294112
codesusuzz <hluvee@naver.com>
2022-11-05 21:47:19 +0900

    게시글 작성 성공 ...

--- src/main/java/com/oxksusu/back/api/controller/PostController.java
+++ src/main/java/com/oxksusu/back/api/controller/PostController.java
@@ -1,48 +1,53 @@
 package com.oxksusu.back.api.controller;
 
+import com.oxksusu.back.api.entity.Posts;
 import com.oxksusu.back.api.service.dto.PostModifyDto;
 import com.oxksusu.back.api.service.UserService;
 import com.oxksusu.back.api.service.dto.PostWriteDto;
 import com.oxksusu.back.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 
 // 요청으로 author 정보가 넘어오면 게시글 정보를 읽어옵니다.
 @RestController
-@RequestMapping("/api/v1/article")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class PostController {
 
     private final UserService userService;
 
-    @GetMapping() // 전체 게시글 목록 조회 (최신순)
+    @GetMapping("/article") // 전체 게시글 목록 조회 (최신순)
     public ApiResponse readPostList() {
-        return ApiResponse.success("posts", userService.getPosts());
+
+        List<Posts> posts = userService.getPosts();
+        return ApiResponse.success("posts", posts);
     }
 
-    @GetMapping() // 한 사람이 쓴 게시글 전체 목록 조회 (최신순)
+    @GetMapping("/article/{nickname}")// 한 사람이 쓴 게시글 전체 목록 조회 (최신순)
     public ApiResponse readPostList(HttpServletRequest request,
                                     HttpServletResponse response,
-                                    @RequestParam("nickname") String nickname) {
+                                    @PathVariable String nickname) {
 
         return ApiResponse.success("posts", userService.getPostsByNickname(nickname));
     }
 
-    @GetMapping() // 게시글 번호로 게시글 조회
+    @GetMapping("/article/{articleNo}") // 게시글 번호로 게시글 조회
     public ApiResponse readPost(HttpServletRequest request,
                                 HttpServletResponse response,
-                                @RequestParam("articleNo") Long articleNo) {
+                                @PathVariable Long articleNo) {
         return ApiResponse.success("posts", userService.getPostByArticleNo(articleNo));
     }
 
-    @PostMapping("/modify") // 게시글 번호로 게시글 수정
+    @PostMapping("/modify/{articleNo}") // 게시글 번호로 게시글 수정
     public ApiResponse modifyPost(HttpServletRequest request,
                                   HttpServletResponse response,
-                                  @RequestParam("articleNo") Long articleNo,
+                                  @PathVariable Long articleNo,
                                   @RequestBody PostModifyDto modifyDto) {
 
         String msg = userService.modifyPost(articleNo, modifyDto);
