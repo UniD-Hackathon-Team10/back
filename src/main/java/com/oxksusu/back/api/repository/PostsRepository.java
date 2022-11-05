10957035e73c2138b83a98b600e0063e51e3c297
kimcodell <kimminhyuk0719@gmail.com>
2022-11-06 08:56:35 +0900

    add post write

--- src/main/java/com/oxksusu/back/api/repository/PostsRepository.java
+++ src/main/java/com/oxksusu/back/api/repository/PostsRepository.java
@@ -2,6 +2,7 @@ package com.oxksusu.back.api.repository;
 
 import com.oxksusu.back.api.entity.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,10 @@ import java.util.List;
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     @Query(value = "SELECT * FROM posts p join book_table b on p.book_title = b.book_title where p.article_no = ?1", nativeQuery = true)
     Posts findPostDetail(Long article_no);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "INSERT INTO posts(author,book_thumbnail,book_title,category,content,nickname,user_id) VALUES (?1,?2,?3,?4,?5,?6,?7)", nativeQuery = true)
+    void savePost(String author, String bookTumbnail, String bookTitle, String category, String content, String nickname, String userId);
 
     List<Posts> findAllByUserId(String userId);
 
